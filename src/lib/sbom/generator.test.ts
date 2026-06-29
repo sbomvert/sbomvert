eeb4f571500a39ca8831d3475f529dfad2b3e3eb
Nathnael Tadesse <nmktad@gmail.com>
2026-06-29 10:24:13 +0200

    feat: add new function for cyclonedx generation

--- src/lib/sbom/generator.test.ts
+++ src/lib/sbom/generator.test.ts
@@ -1,8 +1,8 @@
-import { GenerateSPDXSBOMwithTool } from './generator';
+import { GenerateCycloneDXSBOMwithTool, GenerateSPDXSBOMwithTool } from './generator';
 
 describe('GenerateSPDXSBOMwithTool', () => {
   test('returns correct command for trivy', () => {
-    expect(GenerateSPDXSBOMwithTool('trivy', 'myimage')).toStrictEqual({"args": ["image", "--format spdx-json", "myimage"], "cmd": "trivy"});
+    expect(GenerateSPDXSBOMwithTool('trivy', 'myimage')).toStrictEqual({"args": ["image", "--format", "spdx-json", "myimage"], "cmd": "trivy"});
   });
 
   test('returns correct command for syft', () => {
@@ -15,5 +15,23 @@ describe('GenerateSPDXSBOMwithTool', () => {
 
   test('returns null for unknown tool', () => {
     expect(GenerateSPDXSBOMwithTool('unknown', 'myimage')).toBeNull();
+  });
+});
+
+describe('GenerateCycloneDXSBOMwithTool', () => {
+  test('returns correct command for trivy', () => {
+    expect(GenerateCycloneDXSBOMwithTool('trivy', 'myimage')).toStrictEqual({"args": ["image", "--format", "cyclonedx", "myimage"], "cmd": "trivy"});
+  });
+
+  test('returns correct command for syft', () => {
+    expect(GenerateCycloneDXSBOMwithTool('syft', 'myimage')).toStrictEqual({"args": ["myimage", "-o=cyclonedx-json"], "cmd": "syft"});
+  });
+
+  test('returns correct command for scout', () => {
+    expect(GenerateCycloneDXSBOMwithTool('scout', 'myimage')).toStrictEqual({"args": ["scout", "sbom", "myimage", "--format=cyclonedx"], "cmd": "docker"});
+  });
+
+  test('returns null for unknown tool', () => {
+    expect(GenerateCycloneDXSBOMwithTool('unknown', 'myimage')).toBeNull();
   });
 });
--- src/lib/sbom/generator.ts
+++ src/lib/sbom/generator.ts
@@ -15,3 +15,18 @@ export function GenerateSPDXSBOMwithTool(tool: string, image: string): { cmd: st
   }
 }
 
+export function GenerateCycloneDXSBOMwithTool(tool: string, image: string): { cmd: string; args: string[] } | null {
+  switch (tool) {
+    case 'trivy':
+      return {cmd: 'trivy',args: ['image','--format','cyclonedx',image]}
+
+    case 'syft':
+      return {cmd: 'syft',args: [image,'-o=cyclonedx-json']}
+
+    case 'scout':
+      return  {cmd: 'docker',args: ['scout','sbom',image,'--format=cyclonedx']}
+
+    default:
+      return null;
+  }
+}
